0bcc8566e2cc219a19e6a3c889e2c29b3eb1ba00
Ivica Batinić <ivicabatinic123@gmail.com>
2023-11-12 13:23:37 +0100

    update

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ A Typescript CLI starter template.
 Includes:
 
 - [TypeScript](https://www.typescriptlang.org/), for writing good code
-- [Ava](https://www.npmjs.com/package/ava), for writing good tests
+- [Node Test Runner](https://nodejs.org/api/test.html), for writing good tests
 - [Commander](https://www.npmjs.com/package/commander), for building CLI applications
 - [Pkg](https://www.npmjs.com/package/pkg), for building cross-platform native executables
 
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,20 @@
-import test from 'ava'
+import assert from "assert/strict";
+import test from "node:test";
 
-import { orderPizza } from './index'
+import { orderPizza } from "./index";
 
-test.before(async t => {
-  console.log('Starting tests!')
-})
+test.before(async (t) => {
+  console.log("Starting tests!");
+});
 
-test('can order a pizza', async t => {
-
+test("can order a pizza", async (t) => {
   let result = await orderPizza({
     peppers: false,
     pineapple: false,
     bbqSauce: false,
-    cheeseType: 'swiss'
-  })
+    cheeseType: "swiss",
+  });
 
-  t.true(result.message.includes('you ordered a pizza'))
-  t.true(result.message.includes('swiss'))
-
-})
+  assert.strictEqual(result.message.includes("you ordered a pizza"), true);
+  assert.strictEqual(result.message.includes("swiss"), true);
+});
